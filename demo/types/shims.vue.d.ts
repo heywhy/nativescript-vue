edfc2759c296a0995fe99f9287458e976b248fa9
atanda rasheed <atandarash@gmail.com>
2023-04-10 01:20:16 +0100

    chore: update demo dependencies and types

--- demo/types/shims.vue.d.ts
+++ demo/types/shims.vue.d.ts
@@ -1,5 +1,5 @@
 declare module "*.vue" {
-  import type { DefineComponent } from "../../dist/withCompiler";
+  import type { DefineComponent } from '@vue/runtime-core';
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
