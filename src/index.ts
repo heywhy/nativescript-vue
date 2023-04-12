d964a86c4f08b976951fc9a00a5b75e49f1edd9b
atanda rasheed <atandarash@gmail.com>
2023-04-12 12:15:49 +0100

    fix: resolve vscode ext errors and extend exports

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,7 @@ init();
 export * from "./dom";
 export * from "./registry";
 export * from "./renderer";
+export { ListItem } from './components/ListView'
 export { createNativeView } from "./runtimeHelpers";
 
 export * from "@vue/runtime-core";
